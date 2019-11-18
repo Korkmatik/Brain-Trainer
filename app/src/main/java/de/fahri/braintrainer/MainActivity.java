71838bd0aae088dbb358968cc524d003c190b9e7
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2019-11-18 20:18:45 +0100

    Programmed Go Button Logic

--- app/src/main/java/de/fahri/braintrainer/MainActivity.java
+++ app/src/main/java/de/fahri/braintrainer/MainActivity.java
@@ -1,14 +1,53 @@
 package de.fahri.braintrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button[] btnAnswers;
+    private TextView txtTime;
+    private TextView txtResult;
+    private TextView txtFeedback;
+    private Button btnRestartGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initGoScreen();
+        initGameScreen();
+    }
+
+    private void initGoScreen() {
+        Button btnGo = findViewById(R.id.btnGo);
+        final ConstraintLayout gameLayout = findViewById(R.id.gameLayout);
+
+        gameLayout.setVisibility(View.INVISIBLE);
+        btnGo.setVisibility(View.VISIBLE);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setVisibility(View.INVISIBLE);
+                gameLayout.setVisibility(View.VISIBLE);
+                startGame();
+            }
+        });
+    }
+
+    private void startGame() {
+    }
+
+    private void initGameScreen() {
+    }
+
+    private void initTimer() {
+
     }
 }
