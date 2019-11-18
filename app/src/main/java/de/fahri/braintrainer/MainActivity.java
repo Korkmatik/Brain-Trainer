5c5c8f2117431e35cb9ee27b477a8e751eafb687
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2019-11-18 21:27:43 +0100

    Implemented game logic

--- app/src/main/java/de/fahri/braintrainer/MainActivity.java
+++ app/src/main/java/de/fahri/braintrainer/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView txtTime;
@@ -19,10 +21,55 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtFeedback;
     private Button btnRestartGame;
 
-    private long gameDuration = 3000; // Duration of the game in milliseconds
+    private Random random;
+
+    private long gameDuration = 30_000; // Duration of the game in milliseconds
     private int numberQuestionProcessed;
     private int numberRightAnswers;
+    private int streakWrongAnswers;
+    private int operand1, operand2, result;
     private boolean isGameOver = true;
+
+    public void onAnswerClicked(View view) {
+        if (isGameOver)
+            return;
+
+        Button answerBtn = (Button)view;
+        int answer = Integer.parseInt(answerBtn.getText().toString());
+
+        numberQuestionProcessed += 1;
+
+        if (answer == result)
+            rightAnswer();
+        else
+            wrongAnswer();
+
+        createNewEquation();
+    }
+
+    private void rightAnswer() {
+        numberRightAnswers += 1;
+
+        setFeedbackText("Right!");
+        setScoreText();
+    }
+
+    private void wrongAnswer() {
+        setFeedbackText("Wrong!");
+
+        streakWrongAnswers += 1;
+
+        if (streakWrongAnswers >= 5) {
+            streakWrongAnswers = 0;
+            numberRightAnswers -= 2;
+
+            if (numberRightAnswers < 0) {
+                numberRightAnswers = 0;
+            }
+        }
+
+        setScoreText();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,17 +94,92 @@ public class MainActivity extends AppCompatActivity {
                 startGame();
             }
         });
+
+        Log.i("Init", "Go button initialized");
     }
 
     private void startGame() {
         isGameOver = false;
+
+        btnRestartGame.setVisibility(View.INVISIBLE);
+        setStartFeedbackText();
+
+        initTimer();
+        createNewEquation();
+        initScore();
+
+        streakWrongAnswers = 0;
+
+        Log.i("Game", "Game started");
+    }
+
+    private void setStartFeedbackText() {
+        setFeedbackText("Let's Go!");
+    }
+
+    private void createNewEquation() {
+        operand1 = generateRandomOperand();
+        operand2 = generateRandomOperand();
+        result = operand1 + operand2;
+
+        setAnswers();
+
+        setEquationText();
+    }
+
+    private void setAnswers() {
+        int randomRightAnswer = random.nextInt(btnAnswers.length);
+
+        int answer = 0;
+
+        for (int i = 0; i < btnAnswers.length; i++) {
+            if (i == randomRightAnswer) {
+                answer = result;
+            }
+            else {
+                do {
+                    answer = random.nextInt(result) + random.nextInt(20) + 1;
+                } while (answer == result);
+            }
+
+            btnAnswers[i].setText(Integer.toString(answer));
+        }
+    }
+
+    private void setEquationText() {
+        String equationTxt = String.format("%2d + %2d", operand1, operand2);
+
+        txtEquation.setText(equationTxt);
+    }
+
+    private int generateRandomOperand() {
+        return random.nextInt(100);
+    }
+
+    private void initScore() {
+        numberQuestionProcessed = 0;
+        numberRightAnswers = 0;
+
+        setScoreText();
+    }
+
+    private void setScoreText() {
+        String scoreTxt = String.format("%d/%d", numberRightAnswers, numberQuestionProcessed);
+
+        txtScore.setText(scoreTxt);
     }
 
     private void initGameScreen() {
         initViewVariables();
 
+        random = new Random();
+
         initRestartButton();
         initAnswerButtons();
+
+        setStartFeedbackText();
+
+        Log.i("Init", "Game screen initialized");
     }
 
     private void initViewVariables() {
@@ -77,6 +199,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void initRestartButton() {
         btnRestartGame.setVisibility(View.INVISIBLE);
+
+        btnRestartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
     }
 
     private void initAnswerButtons() {
@@ -101,6 +230,8 @@ public class MainActivity extends AppCompatActivity {
         isGameOver = true;
         btnRestartGame.setVisibility(View.VISIBLE);
 
+        setFeedbackText("Done!");
+
         Log.i("Game", "Game Over");
     }
 
@@ -110,5 +241,9 @@ public class MainActivity extends AppCompatActivity {
         txtTime.setText(timerTxt);
 
         Log.i("Timer Text", "Set to: " + timerTxt);
+    }
+
+    private void setFeedbackText(String txt) {
+        txtFeedback.setText(txt);
     }
 }
